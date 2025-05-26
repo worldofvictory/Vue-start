ccd97a311cfc763efcdb44f64789330552701469
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-05-26 12:58:34 +0200

    setup-project

---
+++ vite-project/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+ 
+ import {ref} from 'vue';
+ const flag = ref("")
+
+</script>
+
+<template>
+  <!--Esercizio 1-->
+  <!--<div class="container">
+    <button @click="flag =!flag"> Click!</button>
+    <h2 class="title-esercizio1" v-if = "flag">Hello</h2>
+    <h2  v-else> Bye Bye</h2>
+  </div>-->
+  
+    
+</template>
+
+<style scoped>
+.title-esercizio1 {
+  position: relative;
+  background-color: red;
+}
+</style>
