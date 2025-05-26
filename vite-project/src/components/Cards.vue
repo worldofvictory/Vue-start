533bbaf302960e848a0c38d0a26e69c8fe7c63c9
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-05-26 14:45:16 +0200

    input json

--- vite-project/src/components/Cards.vue
+++ vite-project/src/components/Cards.vue
@@ -4,6 +4,16 @@
  
  const text = ref("")
 
+
+
+const arrayCard = [
+    { name: "user1", age: 12, src: 'https://placekitten.com/801/800', alt: "kitten-1"},
+    { name: "user2", age: 15, src: 'https://placekitten.com/802/800', alt: "kitten-2"},
+    { name: "user3", age: 8, src: 'https://placekitten.com/803/800', alt: "kitten-3"},
+    { name: "user4", age: 10, src: 'https://placekitten.com/804/800', alt: "kitten-4"},
+    { name: "user5", age: 15, src: 'https://placekitten.com/805/800', alt: "kitten-5"},
+    { name: "user6", age: 17, src: 'https://placekitten.com/806/800', alt: "kitten-6"},
+]
 </script>
 
 <template>
