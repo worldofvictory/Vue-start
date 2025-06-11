0b1c7d6052c02318e7bc60089ec5808a936de402
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-06-11 09:27:00 +0200

    notification card

--- vite-project/src/components/Computed.vue
+++ vite-project/src/components/Computed.vue
@@ -1,24 +1,61 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
-import { computed,ref } from 'vue';
-const originalPrice = ref(100);
-const discount = ref();
-const finalPrice = computed(() => {
-  return originalPrice.value - discount.value ;
-});
+type NotificationType = 'email' | 'web' | 'telegram'
 
+const notificationOptions: NotificationType[] = ['email', 'web', 'telegram']
 
+const notificationLabels: Record<NotificationType, string> = {
+  email: 'Email',
+  web: 'Web',
+  telegram: 'Telegram',
+}
+
+// Дані, які обрав користувач
+const selectedNotifications = ref<NotificationType[]>([])
+
+// Обробка форми
+function handleSubmit() {
+  console.log('Вибрані варіанти:', selectedNotifications.value)
+  // на наступному етапі реалізуємо fetch POST
+}
 </script>
 
+
 <template>
-  <div class="price-block">
-    <input type="number" v-model="discount" placeholder="Original Price" />
-    <p> Original Price: {{ originalPrice }}</p>
-    <p> Discount: {{ discount }}</p>
-    <p> Final Price: {{ finalPrice }}</p>
+  <div class="notification-settings">
+    <h2>Scegli il tuo metodo di notificazione</h2>
+
+    <form @submit.prevent="handleSubmit">
+      <div class="notification-option" v-for="option in notificationOptions" :key="option">
+        <label>
+          <input
+            type="checkbox"
+            :value="option"
+            v-model="selectedNotifications"
+          />
+          {{ notificationLabels[option] }}
+        </label>
+      </div>
+
+      <button type="submit">Salva</button>
+    </form>
   </div>
 </template>
 
-<style scoped>
 
+<style scoped>
+.notification-settings {
+  max-width: 100%;
+  margin: 2rem auto;
+  padding: 60px;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+}
+.notification-option {
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: left;
+}
 </style>
+
